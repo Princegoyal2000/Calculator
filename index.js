0a9a89246240be245eee338c161e491ecd1c7b95
Prince Goyal <princegoyalpr2000@gmail.com>
2022-01-26 14:53:09 +0530

    Calculator intial try

---
+++ index.js
@@ -0,0 +1,30 @@
+var screen = document.getElementById('screen');
+var buttons = document.querySelectorAll('button');
+let screenValue='';
+for(item of buttons)
+{
+item.addEventListener('click',(event)=>{
+  buttonText = event.target.innerText;
+  console.log("Button text is ", buttonText);
+  if(buttonText == 'X')
+  {
+    buttonText='*';
+    screenValue += buttonText;
+    screen.value = screenValue ;
+  }
+  else if(buttonText == 'C' )
+  {
+    screenValue = "";
+    screen.value = screenValue ;
+  }
+  else if(buttonText == '=' )
+  {
+    screen.value = eval(screenValue)
+  }
+  else{
+    screenValue += buttonText;
+    screen.value = screenValue ;
+  }
+
+})
+}
